75ce7cda46d2d2242d1edcfdb9a5380912deec36
kshitish <kshitish182@gmail.com>
2020-07-21 08:44:40 +0545

    Updates

---
+++ precache-manifest.a5c4b6b89f88b5a9dc229f042cd6b991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eaca3a6389a1da5a289f7fec83d1b2a",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "b28e675c2d6f1ac296fc",
+    "url": "/ToDo/static/css/main.2b76b780.chunk.css"
+  },
+  {
+    "revision": "f618c4c0a20d4a4718be",
+    "url": "/ToDo/static/js/2.2cbc9676.chunk.js"
+  },
+  {
+    "revision": "44b724887c788dd91af1f626e24324fa",
+    "url": "/ToDo/static/js/2.2cbc9676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28e675c2d6f1ac296fc",
+    "url": "/ToDo/static/js/main.fb9837d8.chunk.js"
+  },
+  {
+    "revision": "a64d1f8367d1550a5ad8",
+    "url": "/ToDo/static/js/runtime-main.e935b743.js"
+  }
+]);
